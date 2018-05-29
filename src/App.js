bb33b36259879686c79b68d32c5dfc2d3110e8e4
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-05-29 07:07:27 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import DevTools from './DevTools';
+
+const App = () => {
+  return (
+    <div className="App">
+      <CommentsListContainer />
+      <DevTools />
+    </div>
+  );
+};
+
+export default App;
